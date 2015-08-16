0d03531ed5f0db4f0391f18e53c51c37b6f62b06
Waqas <wikkii@msn.com>
2015-08-16 05:18:02 -0400

    sadf

--- app/src/main/java/com/example/francisco/drowningdetector/Main.java
+++ app/src/main/java/com/example/francisco/drowningdetector/Main.java
@@ -1,294 +1,294 @@
-package com.example.francisco.drowningdetector;
-
-import android.graphics.Bitmap;
-
-import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Vector;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.video.BackgroundSubtractorMOG2;
-import org.opencv.video.Video;
-import org.opencv.videoio.VideoCapture;
-
-
-public class Main {
-	static {
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		// System.loadLibrary("opencv_java2410");
-	}
-
-	static Mat imag = null;
-	static Mat orgin = null;
-	static Mat kalman = null;
-	public static Tracker tracker;
-
-	public static void main(String[] args) throws InterruptedException {
-
-		if (args.length>0){
-			CONFIG.filename = args[0];
-		}
-
-		JFrame jFrame = new JFrame("MULTIPLE-TARGET TRACKING");
-		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JLabel vidpanel = new JLabel();
-		jFrame.setContentPane(vidpanel);
-		jFrame.setSize(CONFIG.FRAME_WIDTH, CONFIG.FRAME_HEIGHT);
-		jFrame.setLocation((3 / 4)
-				* Toolkit.getDefaultToolkit().getScreenSize().width, (3 / 4)
-				* Toolkit.getDefaultToolkit().getScreenSize().height);
-		jFrame.setVisible(true);
-
-		// ////////////////////////////////////////////////////////
-		JFrame jFrame2 = new JFrame("BACKGROUND SUBSTRACTION");
-		jFrame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JLabel vidpanel2 = new JLabel();
-		jFrame2.setContentPane(vidpanel2);
-		jFrame2.setSize(CONFIG.FRAME_WIDTH, CONFIG.FRAME_HEIGHT);
-		jFrame2.setLocation(
-				Toolkit.getDefaultToolkit().getScreenSize().width / 2, (3 / 4)
-						* Toolkit.getDefaultToolkit().getScreenSize().height);
-		jFrame2.setVisible(true);
-		// ////////////////////////////////////////////////////////
-
-		// ////////////////////////////////////////////////////////
-		JFrame jFrame3 = new JFrame("VIDEO INPUT");
-		jFrame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JLabel vidpanel3 = new JLabel();
-		jFrame3.setContentPane(vidpanel3);
-		jFrame3.setSize(CONFIG.FRAME_WIDTH, CONFIG.FRAME_HEIGHT);
-		jFrame3.setLocation((3 / 4)
-				* Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit
-				.getDefaultToolkit().getScreenSize().height / 2);
-		jFrame3.setVisible(true);
-		// ////////////////////////////////////////////////////////
-/*
-		// ////////////////////////////////////////////////////////
-		JFrame jFrame4 = new JFrame("KALMAN FILTER");
-		jFrame4.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JLabel vidpanel4 = new JLabel();
-		jFrame4.setContentPane(vidpanel4);
-		jFrame4.setSize(CONFIG.FRAME_WIDTH, CONFIG.FRAME_HEIGHT);
-		jFrame4.setLocation(
-				Toolkit.getDefaultToolkit().getScreenSize().width / 2, Toolkit
-						.getDefaultToolkit().getScreenSize().height / 2);
-		jFrame4.setVisible(false);
-		// ////////////////////////////////////////////////////////
-*/
-		Mat frame = new Mat();
-		Mat outbox = new Mat();
-		Mat diffFrame = null;
-		Vector<Rect> array = new Vector<Rect>();
-
-		BackgroundSubtractorMOG2 mBGSub = Video
-				.createBackgroundSubtractorMOG2();
-
-		tracker = new Tracker((float) CONFIG._dt,
-				(float) CONFIG._Accel_noise_mag, CONFIG._dist_thres,
-				CONFIG._maximum_allowed_skipped_frames,
-				CONFIG._max_trace_length);
-
-		// Thread.sleep(1000);
-		VideoCapture camera = new VideoCapture("visiontraffic.avi");
-		//camera.open(CONFIG.filename);
-		// VideoCapture camera = new VideoCapture(0);
-		int i = 0;
-
-		if (!camera.isOpened()) {
-			System.out.print("Can not open Camera, try it later.");
-			return;
-		}
-
-		while (true) {
-			if (!camera.read(frame))
-				break;
-			Imgproc.resize(frame, frame, new Size(CONFIG.FRAME_WIDTH, CONFIG.FRAME_HEIGHT),
-					0., 0., Imgproc.INTER_LINEAR);
-			imag = frame.clone();
-			orgin = frame.clone();
-			kalman = frame.clone();
-			if (i == 0) {
-				// jFrame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-				diffFrame = new Mat(outbox.size(), CvType.CV_8UC1);
-				diffFrame = outbox.clone();
-			}
-
-			if (i == 1) {
-				diffFrame = new Mat(frame.size(), CvType.CV_8UC1);
-				processFrame(camera, frame, diffFrame, mBGSub);
-				frame = diffFrame.clone();
-/*
-				array = detectionContours(diffFrame);
-				// ///////
-				Vector<Point> detections = new Vector<>();
-				// detections.clear();
-				Iterator<Rect> it = array.iterator();
-				while (it.hasNext()) {
-					Rect obj = it.next();
-
-					int ObjectCenterX = (int) ((obj.tl().x + obj.br().x) / 2);
-					int ObjectCenterY = (int) ((obj.tl().y + obj.br().y) / 2);
-
-					Point pt = new Point(ObjectCenterX, ObjectCenterY);
-					detections.add(pt);
-				}
-*/
-				// ///////
-/*
-				if (array.size() > 0) {
-					tracker.update(array, detections, imag);
-					Iterator<Rect> it3 = array.iterator();
-					while (it3.hasNext()) {
-						Rect obj = it3.next();
-
-						int ObjectCenterX = (int) ((obj.tl().x + obj.br().x) / 2);
-						int ObjectCenterY = (int) ((obj.tl().y + obj.br().y) / 2);
-
-						Point pt = new Point(ObjectCenterX, ObjectCenterY);
-
-						Imgproc.rectangle(imag, obj.br(), obj.tl(), new Scalar(
-								0, 255, 0), 2);
-						Imgproc.circle(imag, pt, 1, new Scalar(0, 0, 255), 2);
-					}
-				} else if (array.size() == 0) {
-					tracker.updateKalman(imag, detections);
-				}
-
-				for (int k = 0; k < tracker.tracks.size(); k++) {
-					int traceNum = tracker.tracks.get(k).trace.size();
-					if (traceNum > 1) {
-						for (int jt = 1; jt < tracker.tracks.get(k).trace
-								.size(); jt++) {
-							Imgproc.line(imag,
-									tracker.tracks.get(k).trace.get(jt - 1),
-									tracker.tracks.get(k).trace.get(jt),
-									CONFIG.Colors[tracker.tracks.get(k).track_id % 9],
-									2, 4, 0);
-						}
-					}
-				}
-*/
-/*
-				Imgproc.putText(imag, "Input: " + CONFIG.filename, new Point(20, 360),
-						Core.FONT_HERSHEY_PLAIN, 1, new Scalar(255, 255, 255),
-						1);
-				Imgproc.putText(imag,
-						"So track hien tai: " + tracker.tracks.size()
-								+ "     Da xoa: " + tracker.track_removed,
-						new Point(20, 50), Core.FONT_HERSHEY_PLAIN, 1,
-						new Scalar(255, 255, 255), 1); */
-			}
-
-			i = 1;
-
-			ImageIcon image = new ImageIcon(Mat2bufferedImage(imag));
-			vidpanel.setIcon(image);
-			vidpanel.repaint();
-			// temponFrame = outerBox.clone();
-
-			ImageIcon image2 = new ImageIcon(Mat2bufferedImage(frame));
-			vidpanel2.setIcon(image2);
-			vidpanel2.repaint();
-
-			ImageIcon image3 = new ImageIcon(Mat2bufferedImage(orgin));
-			vidpanel3.setIcon(image3);
-			vidpanel3.repaint();
-/*
-			ImageIcon image4 = new ImageIcon(Mat2bufferedImage(kalman));
-			vidpanel4.setIcon(image4);
-			vidpanel4.repaint();
-*/
-		}
-
-	}
-
-	// background substractionMOG2
-	protected static void processFrame(VideoCapture capture, Mat mRgba,
-									   Mat mFGMask, BackgroundSubtractorMOG2 mBGSub) {
-		// GREY_FRAME also works and exhibits better performance
-		mBGSub.apply(mRgba, mFGMask, CONFIG.learningRate);
-		Imgproc.cvtColor(mFGMask, mRgba, Imgproc.COLOR_GRAY2BGRA, 0);
-		Mat erode = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(
-				8, 8));
-		Mat dilate = Imgproc.getStructuringElement(Imgproc.MORPH_RECT,
-				new Size(8, 8));
-
-		Mat openElem = Imgproc.getStructuringElement(Imgproc.MORPH_RECT,
-				new Size(3, 3), new Point(1, 1));
-		Mat closeElem = Imgproc.getStructuringElement(Imgproc.MORPH_RECT,
-				new Size(7, 7), new Point(3, 3));
-
-		Imgproc.threshold(mFGMask, mFGMask, 127, 255, Imgproc.THRESH_BINARY);
-		Imgproc.morphologyEx(mFGMask, mFGMask, Imgproc.MORPH_OPEN, erode);
-		Imgproc.morphologyEx(mFGMask, mFGMask, Imgproc.MORPH_OPEN, dilate);
-		Imgproc.morphologyEx(mFGMask, mFGMask, Imgproc.MORPH_OPEN, openElem);
-		Imgproc.morphologyEx(mFGMask, mFGMask, Imgproc.MORPH_CLOSE, closeElem);
-	}
-
-	private static Bitmap Mat2bufferedImage(Mat image) {
-		MatOfByte bytemat = new MatOfByte();
-		Imgcodecs.imencode(".jpg", image, bytemat);
-		byte[] bytes = bytemat.toArray();
-		InputStream in = new ByteArrayInputStream(bytes);
-		BufferedImage img = null;
-		try {
-			img = ImageIO.read(in);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return img;
-	}
-/*
-	public static Vector<Rect> detectionContours(Mat outmat) {
-		Mat v = new Mat();
-		Mat vv = outmat.clone();
-		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-		Imgproc.findContours(vv, contours, v, Imgproc.RETR_LIST,
-				Imgproc.CHAIN_APPROX_SIMPLE);
-
-		int maxAreaIdx = -1;
-		Rect r = null;
-		Vector<Rect> rect_array = new Vector<Rect>();
-
-		for (int idx = 0; idx < contours.size(); idx++) {
-			Mat contour = contours.get(idx);
-			double contourarea = Imgproc.contourArea(contour);
-			if (contourarea > CONFIG.MIN_BLOB_AREA && contourarea < CONFIG.MAX_BLOB_AREA) {
-				// MIN_BLOB_AREA = contourarea;
-				maxAreaIdx = idx;
-				r = Imgproc.boundingRect(contours.get(maxAreaIdx));
-				rect_array.add(r);
-				// Imgproc.drawContours(imag, contours, maxAreaIdx, new
-				// Scalar(255, 255, 255));
-			}
-
-		}
-
-		v.release();
-		return rect_array;
-	}
-*/
-
-}
+//package com.example.francisco.drowningdetector;
+//
+//import android.graphics.Bitmap;
+//
+//import java.awt.Toolkit;
+//import java.awt.image.BufferedImage;
+//import java.io.ByteArrayInputStream;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.ArrayList;
+//import java.util.Iterator;
+//import java.util.List;
+//import java.util.Vector;
+//
+//import javax.imageio.ImageIO;
+//import javax.swing.ImageIcon;
+//import javax.swing.JFrame;
+//import javax.swing.JLabel;
+//
+//import org.opencv.core.Core;
+//import org.opencv.core.CvType;
+//import org.opencv.core.Mat;
+//import org.opencv.core.MatOfByte;
+//import org.opencv.core.MatOfPoint;
+//import org.opencv.core.Point;
+//import org.opencv.core.Rect;
+//import org.opencv.core.Scalar;
+//import org.opencv.core.Size;
+//import org.opencv.imgcodecs.Imgcodecs;
+//import org.opencv.imgproc.Imgproc;
+//import org.opencv.video.BackgroundSubtractorMOG2;
+//import org.opencv.video.Video;
+//import org.opencv.videoio.VideoCapture;
+//
+//
+//public class Main {
+//	static {
+//		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//		// System.loadLibrary("opencv_java2410");
+//	}
+//
+//	static Mat imag = null;
+//	static Mat orgin = null;
+//	static Mat kalman = null;
+//	public static Tracker tracker;
+//
+//	public static void main(String[] args) throws InterruptedException {
+//
+//		if (args.length>0){
+//			CONFIG.filename = args[0];
+//		}
+//
+//		JFrame jFrame = new JFrame("MULTIPLE-TARGET TRACKING");
+//		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		JLabel vidpanel = new JLabel();
+//		jFrame.setContentPane(vidpanel);
+//		jFrame.setSize(CONFIG.FRAME_WIDTH, CONFIG.FRAME_HEIGHT);
+//		jFrame.setLocation((3 / 4)
+//				* Toolkit.getDefaultToolkit().getScreenSize().width, (3 / 4)
+//				* Toolkit.getDefaultToolkit().getScreenSize().height);
+//		jFrame.setVisible(true);
+//
+//		// ////////////////////////////////////////////////////////
+//		JFrame jFrame2 = new JFrame("BACKGROUND SUBSTRACTION");
+//		jFrame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		JLabel vidpanel2 = new JLabel();
+//		jFrame2.setContentPane(vidpanel2);
+//		jFrame2.setSize(CONFIG.FRAME_WIDTH, CONFIG.FRAME_HEIGHT);
+//		jFrame2.setLocation(
+//				Toolkit.getDefaultToolkit().getScreenSize().width / 2, (3 / 4)
+//						* Toolkit.getDefaultToolkit().getScreenSize().height);
+//		jFrame2.setVisible(true);
+//		// ////////////////////////////////////////////////////////
+///*
+//		// ////////////////////////////////////////////////////////
+//		JFrame jFrame3 = new JFrame("VIDEO INPUT");
+//		jFrame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		JLabel vidpanel3 = new JLabel();
+//		jFrame3.setContentPane(vidpanel3);
+//		jFrame3.setSize(CONFIG.FRAME_WIDTH, CONFIG.FRAME_HEIGHT);
+//		jFrame3.setLocation((3 / 4)
+//				* Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit
+//				.getDefaultToolkit().getScreenSize().height / 2);
+//		jFrame3.setVisible(true);
+//		// ////////////////////////////////////////////////////////
+//
+//		// ////////////////////////////////////////////////////////
+//		JFrame jFrame4 = new JFrame("KALMAN FILTER");
+//		jFrame4.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		JLabel vidpanel4 = new JLabel();
+//		jFrame4.setContentPane(vidpanel4);
+//		jFrame4.setSize(CONFIG.FRAME_WIDTH, CONFIG.FRAME_HEIGHT);
+//		jFrame4.setLocation(
+//				Toolkit.getDefaultToolkit().getScreenSize().width / 2, Toolkit
+//						.getDefaultToolkit().getScreenSize().height / 2);
+//		jFrame4.setVisible(false);
+//		// ////////////////////////////////////////////////////////
+//*/
+//		Mat frame = new Mat();
+//		Mat outbox = new Mat();
+//		Mat diffFrame = null;
+//		Vector<Rect> array = new Vector<Rect>();
+//
+//		BackgroundSubtractorMOG2 mBGSub = Video
+//				.createBackgroundSubtractorMOG2();
+//
+//		tracker = new Tracker((float) CONFIG._dt,
+//				(float) CONFIG._Accel_noise_mag, CONFIG._dist_thres,
+//				CONFIG._maximum_allowed_skipped_frames,
+//				CONFIG._max_trace_length);
+//
+//		// Thread.sleep(1000);
+//		VideoCapture camera = new VideoCapture("visiontraffic.avi");
+//		//camera.open(CONFIG.filename);
+//		// VideoCapture camera = new VideoCapture(0);
+//		int i = 0;
+//
+//		if (!camera.isOpened()) {
+//			System.out.print("Can not open Camera, try it later.");
+//			return;
+//		}
+//
+//		while (true) {
+//			if (!camera.read(frame))
+//				break;
+//			Imgproc.resize(frame, frame, new Size(CONFIG.FRAME_WIDTH, CONFIG.FRAME_HEIGHT),
+//					0., 0., Imgproc.INTER_LINEAR);
+//			imag = frame.clone();
+//			orgin = frame.clone();
+//			kalman = frame.clone();
+//			if (i == 0) {
+//				// jFrame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+//				diffFrame = new Mat(outbox.size(), CvType.CV_8UC1);
+//				diffFrame = outbox.clone();
+//			}
+//
+//			if (i == 1) {
+//				diffFrame = new Mat(frame.size(), CvType.CV_8UC1);
+//				processFrame(camera, frame, diffFrame, mBGSub);
+//				frame = diffFrame.clone();
+///*
+//				array = detectionContours(diffFrame);
+//				// ///////
+//				Vector<Point> detections = new Vector<>();
+//				// detections.clear();
+//				Iterator<Rect> it = array.iterator();
+//				while (it.hasNext()) {
+//					Rect obj = it.next();
+//
+//					int ObjectCenterX = (int) ((obj.tl().x + obj.br().x) / 2);
+//					int ObjectCenterY = (int) ((obj.tl().y + obj.br().y) / 2);
+//
+//					Point pt = new Point(ObjectCenterX, ObjectCenterY);
+//					detections.add(pt);
+//				}
+//*/
+//				// ///////
+///*
+//				if (array.size() > 0) {
+//					tracker.update(array, detections, imag);
+//					Iterator<Rect> it3 = array.iterator();
+//					while (it3.hasNext()) {
+//						Rect obj = it3.next();
+//
+//						int ObjectCenterX = (int) ((obj.tl().x + obj.br().x) / 2);
+//						int ObjectCenterY = (int) ((obj.tl().y + obj.br().y) / 2);
+//
+//						Point pt = new Point(ObjectCenterX, ObjectCenterY);
+//
+//						Imgproc.rectangle(imag, obj.br(), obj.tl(), new Scalar(
+//								0, 255, 0), 2);
+//						Imgproc.circle(imag, pt, 1, new Scalar(0, 0, 255), 2);
+//					}
+//				} else if (array.size() == 0) {
+//					tracker.updateKalman(imag, detections);
+//				}
+//
+//				for (int k = 0; k < tracker.tracks.size(); k++) {
+//					int traceNum = tracker.tracks.get(k).trace.size();
+//					if (traceNum > 1) {
+//						for (int jt = 1; jt < tracker.tracks.get(k).trace
+//								.size(); jt++) {
+//							Imgproc.line(imag,
+//									tracker.tracks.get(k).trace.get(jt - 1),
+//									tracker.tracks.get(k).trace.get(jt),
+//									CONFIG.Colors[tracker.tracks.get(k).track_id % 9],
+//									2, 4, 0);
+//						}
+//					}
+//				}
+//*/
+///*
+//				Imgproc.putText(imag, "Input: " + CONFIG.filename, new Point(20, 360),
+//						Core.FONT_HERSHEY_PLAIN, 1, new Scalar(255, 255, 255),
+//						1);
+//				Imgproc.putText(imag,
+//						"So track hien tai: " + tracker.tracks.size()
+//								+ "     Da xoa: " + tracker.track_removed,
+//						new Point(20, 50), Core.FONT_HERSHEY_PLAIN, 1,
+//						new Scalar(255, 255, 255), 1); */
+//			}
+//
+//			i = 1;
+//
+//			ImageIcon image = new ImageIcon(Mat2bufferedImage(imag));
+//			vidpanel.setIcon(image);
+//			vidpanel.repaint();
+//			// temponFrame = outerBox.clone();
+//
+//			ImageIcon image2 = new ImageIcon(Mat2bufferedImage(frame));
+//			vidpanel2.setIcon(image2);
+//			vidpanel2.repaint();
+//
+//			ImageIcon image3 = new ImageIcon(Mat2bufferedImage(orgin));
+//			vidpanel3.setIcon(image3);
+//			vidpanel3.repaint();
+///*
+//			ImageIcon image4 = new ImageIcon(Mat2bufferedImage(kalman));
+//			vidpanel4.setIcon(image4);
+//			vidpanel4.repaint();
+//*/
+//		}
+//
+//	}
+//
+//	// background substractionMOG2
+//	protected static void processFrame(VideoCapture capture, Mat mRgba,
+//									   Mat mFGMask, BackgroundSubtractorMOG2 mBGSub) {
+//		// GREY_FRAME also works and exhibits better performance
+//		mBGSub.apply(mRgba, mFGMask, CONFIG.learningRate);
+//		Imgproc.cvtColor(mFGMask, mRgba, Imgproc.COLOR_GRAY2BGRA, 0);
+//		Mat erode = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(
+//				8, 8));
+//		Mat dilate = Imgproc.getStructuringElement(Imgproc.MORPH_RECT,
+//				new Size(8, 8));
+//
+//		Mat openElem = Imgproc.getStructuringElement(Imgproc.MORPH_RECT,
+//				new Size(3, 3), new Point(1, 1));
+//		Mat closeElem = Imgproc.getStructuringElement(Imgproc.MORPH_RECT,
+//				new Size(7, 7), new Point(3, 3));
+//
+//		Imgproc.threshold(mFGMask, mFGMask, 127, 255, Imgproc.THRESH_BINARY);
+//		Imgproc.morphologyEx(mFGMask, mFGMask, Imgproc.MORPH_OPEN, erode);
+//		Imgproc.morphologyEx(mFGMask, mFGMask, Imgproc.MORPH_OPEN, dilate);
+//		Imgproc.morphologyEx(mFGMask, mFGMask, Imgproc.MORPH_OPEN, openElem);
+//		Imgproc.morphologyEx(mFGMask, mFGMask, Imgproc.MORPH_CLOSE, closeElem);
+//	}
+//
+//	private static Bitmap Mat2bufferedImage(Mat image) {
+//		MatOfByte bytemat = new MatOfByte();
+//		Imgcodecs.imencode(".jpg", image, bytemat);
+//		byte[] bytes = bytemat.toArray();
+//		InputStream in = new ByteArrayInputStream(bytes);
+//		BufferedImage img = null;
+//		try {
+//			img = ImageIO.read(in);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return img;
+//	}
+///*
+//	public static Vector<Rect> detectionContours(Mat outmat) {
+//		Mat v = new Mat();
+//		Mat vv = outmat.clone();
+//		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+//		Imgproc.findContours(vv, contours, v, Imgproc.RETR_LIST,
+//				Imgproc.CHAIN_APPROX_SIMPLE);
+//
+//		int maxAreaIdx = -1;
+//		Rect r = null;
+//		Vector<Rect> rect_array = new Vector<Rect>();
+//
+//		for (int idx = 0; idx < contours.size(); idx++) {
+//			Mat contour = contours.get(idx);
+//			double contourarea = Imgproc.contourArea(contour);
+//			if (contourarea > CONFIG.MIN_BLOB_AREA && contourarea < CONFIG.MAX_BLOB_AREA) {
+//				// MIN_BLOB_AREA = contourarea;
+//				maxAreaIdx = idx;
+//				r = Imgproc.boundingRect(contours.get(maxAreaIdx));
+//				rect_array.add(r);
+//				// Imgproc.drawContours(imag, contours, maxAreaIdx, new
+//				// Scalar(255, 255, 255));
+//			}
+//
+//		}
+//
+//		v.release();
+//		return rect_array;
+//	}
+//
+//
+//}
--- app/src/main/java/com/example/francisco/drowningdetector/MainActivity.java
+++ app/src/main/java/com/example/francisco/drowningdetector/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.francisco.drowningdetector;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -26,24 +27,55 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.video.BackgroundSubtractor;
 import org.opencv.video.BackgroundSubtractorMOG;
 import org.w3c.dom.Text;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class MainActivity extends ActionBarActivity implements CameraBridgeViewBase.CvCameraViewListener2{
 
-    public static final String TAG = "MainActivity";
     public static final String SERVICE_RECEIVER_TAG = "Receiver";
     public static final int SELECT_VIDEO = 1;
 
     static Mat imag = null;
     static Mat orgin = null;
     static Mat kalman = null;
-    public static Tracker tracker;
+    //public static Tracker tracker;
+
+    private static final String    TAG                 = "OCVSample::Activity";
+    private static final Scalar    FACE_RECT_COLOR     = new Scalar(0, 255, 0, 255);
+    public static final int        JAVA_DETECTOR       = 0;
+    public static final int        NATIVE_DETECTOR     = 1;
+
+    private MenuItem               mItemFace50;
+    private MenuItem               mItemFace40;
+    private MenuItem               mItemFace30;
+    private MenuItem               mItemFace20;
+    private MenuItem               mItemType;
+
+    private Mat                    mRgba;
+    private Mat                    mGray;
+    private File mCascadeFile;
+    private CascadeClassifier mJavaDetector;
+    private DetectionBasedTracker  mNativeDetector;
+
+    private int                    mDetectorType       = JAVA_DETECTOR;
+    private String[]               mDetectorName;
+
+    private float                  mRelativeFaceSize   = 0.2f;
+    private int                    mAbsoluteFaceSize   = 0;
+
+    private int					   faceCount = 0;
 
     //this static line is for include directly the opencv library and not use the open cv manager
     static {
@@ -62,6 +94,37 @@ public class MainActivity extends ActionBarActivity implements CameraBridgeViewB
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS: {
                     Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+
+                    try{
+                        InputStream is = getResources().openRawResource(R.raw.cars3);
+                        File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                        mCascadeFile = new File(cascadeDir, "cars3.xml");
+                        FileOutputStream fos = new FileOutputStream(mCascadeFile);
+
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while((bytesRead = is.read(buffer)) != -1){
+                            fos.write(buffer, 0, bytesRead);
+                        }
+                        is.close();
+                        fos.close();
+                        mJavaDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+                        if (mJavaDetector.empty()) {
+                            Log.e(TAG, "Failed to load cascade classifier");
+                            mJavaDetector = null;
+                        } else
+                            Log.i(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
+
+                        mNativeDetector = new DetectionBasedTracker(mCascadeFile.getAbsolutePath(), 0);
+
+                        cascadeDir.delete();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
+                    }
+
                     mOpenCvCameraView.enableView();
                 }
                 break;
@@ -88,15 +151,15 @@ public class MainActivity extends ActionBarActivity implements CameraBridgeViewB
     @Override
     public void onPause() {
         super.onPause();
-//        if (mOpenCvCameraView != null)
-//            mOpenCvCameraView.disableView();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
 //        if (mOpenCvCameraView != null)
-//            mOpenCvCameraView.disableView();
+            mOpenCvCameraView.disableView();
     }
 
     @Override
@@ -133,31 +196,23 @@ public class MainActivity extends ActionBarActivity implements CameraBridgeViewB
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
     }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-
+        mGray = new Mat();
+        mRgba = new Mat();
     }
 
     @Override
     public void onCameraViewStopped() {
-
+        mGray.release();
+        mRgba.release();
     }
 
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return inputFrame.rgba();
-    }
-
-
-    private void showVideoFrames() {
-
-    }
-
-
-    private void resetServiceReceiver() {
-
+       Mat image = inputFrame.rgba();
+        return image;
     }
 
 }
